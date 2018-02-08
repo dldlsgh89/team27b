dd6cf4110464eab6077ad34256c8c80f94edb4b9
Administrator <Administrator@smart20121>
2018-02-08 16:13:05 +0900

    [이인호]드라이브연결중

--- team27b/src/services/AnaunseoDao.java
+++ team27b/src/services/AnaunseoDao.java
@@ -1,5 +1,28 @@
 package services;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
 public class AnaunseoDao {
+	
+	public Connection driverdbcon() throws ClassNotFoundException, SQLException {
+		System.out.println("드라이버로딩 및 db연결메서드 실행");
+		Connection recon = null;
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		String jdbcDriver = "jdbc:mysql://localhost:3306/mysql;jjdev?" +
+				"useUnicode=true&characterEncoding=euckr";
+		String dbUser = "root";
+		String dbPass = "java0000";
+		
+		DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+		System.out.println(DriverManager.getConnection(jdbcDriver, dbUser, dbPass));
+		
+		return recon;
+	}
+	
 
 }
